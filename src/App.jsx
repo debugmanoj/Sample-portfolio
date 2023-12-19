69d88283fc1987cb534f2abb6d7f24b7ee0fc66d
debugmanoj <manojkumarwork01@gmail.com>
2023-12-19 11:44:51 +0530

    Done2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Card from "./components/card"
 
 function App() {
   let a=[{
@@ -25,17 +26,8 @@ function App() {
 <div className="row">
   {
     a.map((e,i)=>{
-      return <div className="col-lg-4 col-sm-6 mb-4" key={i}>
-      <div className="card h-100">
-        <a href="#"><img className="card-img-top" src="https://via.placeholder.com/700x400" alt=""/></a>
-        <div className="card-body">
-          <h4 className="card-title">
-            <a href="#">{e.title}</a>
-          </h4>
-          <p className="card-text">{e.description}</p>
-        </div>
-      </div>
-      </div>
+      return <Card data={e} key={i} />
+      
     })
   }
   
